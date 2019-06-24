ac92d79d8c8a4f949be406cf04a8ce94f743829f
lmxs1237 <yx2478@columbia.edu>
2019-06-24 12:17:08 -0400

    delete test.r

--- test.R
+++
@@ -1,9 +0,0 @@
-library(DBI)
-# Create an ephemeral in-memory RSQLite database
-con <- dbConnect(RSQLite::SQLite(), ":test:")
-
-dbListTables(con)
-
-dbWriteTable(con, "mtcars", "./data/coe.csv")
-dbListTables(con)
-dbReadTable(con, "mtcars")
